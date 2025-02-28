0bc5038f5f991050feea5ceff859a1f2f4b04709
Nabeel Hassan <thenabeelhassan@gmail.com>
2025-02-28 20:05:13 +0500

    Updated terraform script for AWS - EC2 - Docker

--- Terraform/AWS/EC2/Docker/main.tf
+++ Terraform/AWS/EC2/Docker/main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 resource "aws_instance" "docker_ec2" {
-    count = 5
-    ami           = "ami-0b32599a51ef0ad90" # Replace with latest Ubuntu 22.04 AMI for me-central-1
-    instance_type = "t3.micro"
-    key_name      = "DB4"
-    subnet_id     = "subnet-0562ac6e7c427e16c"
-    security_groups = ["sg-0587cfd551e03261c"]
+    count = 2
+    ami           = "ami-0b32599a51ef0ad90" # Replace with latest Ubuntu 22.04 AMI
+    instance_type = "t3.micro" # Replace according to your configuration
+    key_name      = "xxxxx" # Replace according to your configuration
+    subnet_id     = "subnet-xxxxxxxxxxx" # Replace according to your configuration
+    security_groups = ["sg-xxxxxxxxxxxx"] # Replace according to your configuration
     user_data = <<-EOF
         #!/bin/bash
 
@@ -35,5 +35,5 @@ resource "aws_instance" "docker_ec2" {
         # Installing Docker
         apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
     EOF
-    tags = { Name = element(["dockernode1","dockernode2","dockernode3","dockernode4","dockernode5"], count.index) }
+    tags = { Name = element(["dockernode1","dockernode2"], count.index) }
 }
