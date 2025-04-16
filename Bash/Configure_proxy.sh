933f48070be9876bef4e2c67807df0fe4862d180
Nabeel Hassan <thenabeelhassan@gmail.com>
2025-04-17 02:26:37 +0500

    Added Configure Proxy script in Bash

---
+++ Bash/Configure_proxy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+proxy_host="10.0.101.88"
+
+echo "export http_proxy=\"http://$proxy_host:8888\"" >> ./etc/environment
+echo "export https_proxy=\"http://$proxy_host:8888\"" >> ./etc/environment
+echo "export ftp_proxy=\"http://$proxy_host:8888\"" >> ./etc/environment
+echo "export no_proxy=\"localhost,127.0.0.1,::1\"" >> ./etc/environment
+
+echo "Acquire::http::Proxy \"http://$proxy_host:8888/\";" >> ./etc/apt/apt.conf.d/95proxies
+echo "Acquire::https::Proxy \"http://$proxy_host:8888/\";" >> ./etc/apt/apt.conf.d/95proxies
