27fde41905dffd39f4ad5e6ea4a56e7dc418a418
Nabeel Hassan <thenabeelhassan@gmail.com>
2025-06-21 20:20:16 +0500

    Update Configure_proxy.sh

--- Bash/Configure_proxy.sh
+++ Bash/Configure_proxy.sh
@@ -2,10 +2,10 @@
 
 proxy_host="ipaddressofproxyserver"
 
-echo "export http_proxy=\"http://$proxy_host:8888\"" >> ./etc/environment
-echo "export https_proxy=\"http://$proxy_host:8888\"" >> ./etc/environment
-echo "export ftp_proxy=\"http://$proxy_host:8888\"" >> ./etc/environment
-echo "export no_proxy=\"localhost,127.0.0.1,::1\"" >> ./etc/environment
+echo "export http_proxy=\"http://$proxy_host:8888\"" >> /etc/environment
+echo "export https_proxy=\"http://$proxy_host:8888\"" >> /etc/environment
+echo "export ftp_proxy=\"http://$proxy_host:8888\"" >> /etc/environment
+echo "export no_proxy=\"localhost,127.0.0.1,::1\"" >> /etc/environment
 
-echo "Acquire::http::Proxy \"http://$proxy_host:8888/\";" >> ./etc/apt/apt.conf.d/95proxies
-echo "Acquire::https::Proxy \"http://$proxy_host:8888/\";" >> ./etc/apt/apt.conf.d/95proxies
+echo "Acquire::http::Proxy \"http://$proxy_host:8888/\";" >> /etc/apt/apt.conf.d/95proxies
+echo "Acquire::https::Proxy \"http://$proxy_host:8888/\";" >> /etc/apt/apt.conf.d/95proxies
