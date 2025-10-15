e7ee177673c28155f88eaa054040fdea31f35dae
Nabeel Hassan <thenabeelhassan@gmail.com>
2025-10-15 18:27:14 +0500

    Added API Deployment script

---
+++ Bash/API_Deployment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt update
+apt install -y nodejs wget unzip npm
+npm install -g nodemon pm2
+wget https://github.com/thenabeelhassan/testingapi/archive/refs/heads/main.zip
+unzip main.zip
+[ ! -d "/code" ] && sudo mkdir /code
+sudo mv testingapi-main /code/testingapi
+rm -f main.zip
+cd /code/testingapi
+sudo npm i
+pm2 start index.js --name "Testing API"
+pm2 save
