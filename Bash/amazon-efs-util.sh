2572a87201406e9f4f801f3eb8be8b3434dbd0b5
Nabeel Hassan <thenabeelhassan@gmail.com>
2025-10-17 18:53:02 +0500

    added amazon efs script

---
+++ Bash/amazon-efs-util.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+apt -y install git binutils rustc cargo pkg-config libssl-dev gettext
+cd /tmp
+git clone https://github.com/aws/efs-utils
+cd /tmp/efs-utils
+sh ./build-deb.sh
+apt -y install ./build/amazon-efs-utils*deb
